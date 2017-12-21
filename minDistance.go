f4555d80634e51118ea3356405cae772fa94a7f4
caoyongzheng <15267056106@139.com>
2017-12-21 15:15:20 +0800

    minDistance3

--- minDistance.go
+++ minDistance.go
@@ -1,7 +1,5 @@
 package leetcode
 
-import "strings"
-
 // Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
 
 // You have the following 3 operations permitted on a word:
@@ -10,40 +8,33 @@ import "strings"
 // b) Delete a character
 // c) Replace a character
 
+func min(a int, others ...int) int {
+	r := a
+	for _, v := range others {
+		if r > v {
+			r = v
+		}
+	}
+	return r
+}
+
 func minDistance(word1 string, word2 string) int {
-	w1, w2 := word1, word2
-	if len(word1) > len(word2) {
-		w1, w2 = w2, w1
+	state := make([][]int, len(word1)+1)
+	for i := range state {
+		state[i] = make([]int, len(word2)+1)
 	}
-	if len(w1) == 0 {
-		return len(w2)
-	}
-	if len(w1) == 1 {
-		i := strings.Index(w2, string(w1[0]))
-		if i != -1 {
-			return len(w2) - 1
-		}
-		return len(w2)
-	}
-	i1 := strings.Index(w2, string(w1[0]))
-	i2 := strings.LastIndex(w2, string(w1[1]))
-	if i1 == -1 {
-		if i2 == 0 {
-			return minDistance(w1[1:], w2) + 1
-		}
-		return minDistance(w1[1:], w2[1:]) + 1
-	} else if i1 == 0 {
-		return minDistance(w1[1:], w2[1:])
-	} else {
-		if i1 <= len(w2)-len(w1) {
-			if i1 > i2 {
-				return minDistance(w1[1:], w2[1:]) + 1
+	for i := 0; i <= len(word1); i++ {
+		for j := 0; j <= len(word2); j++ {
+			if i == 0 {
+				state[0][j] = j
+			} else if j == 0 {
+				state[i][0] = i
+			} else if word1[i-1] == word2[j-1] {
+				state[i][j] = state[i-1][j-1]
+			} else {
+				state[i][j] = min(state[i-1][j-1], state[i][j-1], state[i-1][j]) + 1
 			}
-			return minDistance(w1, w2[1:]) + 1
 		}
-		if i2 == 0 {
-			return minDistance(w1[1:], w2) + 1
-		}
-		return minDistance(w1[1:], w2[1:]) + 1
 	}
+	return state[len(word1)][len(word2)]
 }
