4c902d52699a88e5abc42ece5b1905e16a915276
caoyongzheng <15267056106@139.com>
2017-12-31 15:14:52 +0800

    add pathSum

---
+++ pathSum_test.go
@@ -0,0 +1,34 @@
+package leetcode
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+func TestPathSum(t *testing.T) {
+	assert.Equal(t, 3, pathSum(&TreeNode{
+		Val: 10,
+		Left: &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 3,
+				},
+				Right: &TreeNode{
+					Val: -2,
+				},
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: -3,
+			Right: &TreeNode{
+				Val: 11,
+			},
+		},
+	}, 8))
+}
