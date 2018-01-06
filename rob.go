0276bee7eeebc7a713f738573d73d2818aea9f68
caoyongzheng <15267056106@139.com>
2018-01-07 00:04:00 +0800

    add rob version3

--- rob.go
+++ rob.go
@@ -7,9 +7,9 @@ package leetcode
 // Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
 
 func rob(nums []int) int {
-	m := make(map[int]int, len(nums))
+	t1, t2 := 0, 0
 	for i := range nums {
-		m[i+1] = max(m[i], m[i-1]+nums[i])
+		t2, t1 = max(t2, t1+nums[i]), t2
 	}
-	return m[len(nums)]
+	return t2
 }
