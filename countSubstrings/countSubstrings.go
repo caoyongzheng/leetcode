cdd398c9b7613351b872160f5043a41aa0a69c76
caoyongzheng <15267056106@139.com>
2018-02-17 10:43:54 +0800

    countSubstrings v2

--- countSubstrings/countSubstrings.go
+++ countSubstrings/countSubstrings.go
@@ -2,16 +2,22 @@ package countSubstrings
 
 func countSubstrings(s string) (c int) {
 	for i := range s {
-		for j := len(s) - 1; j >= i; j-- {
+		for j, o := 0, min(i, len(s)-i-1); j <= o && s[i-j] == s[i+j]; j++ {
 			c++
-			// whether start i and end j is Palindromic
-			for k := 0; k <= (j-i)/2; k++ {
-				if s[i+k] != s[j-k] {
-					c--
-					break
-				}
+		}
+
+		if i < len(s)-1 && s[i] == s[i+1] {
+			for j, o := 0, min(i, len(s)-i-2); j <= o && s[i-j] == s[i+j+1]; j++ {
+				c++
 			}
 		}
 	}
 	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
