26e136efd7a51000ef7ff35f26b1e51edf85d3b7
caoyongzheng <15267056106@139.com>
2018-02-18 09:32:10 +0800

    countSubstrings v3

--- countSubstrings/countSubstrings.go
+++ countSubstrings/countSubstrings.go
@@ -5,11 +5,8 @@ func countSubstrings(s string) (c int) {
 		for j, o := 0, min(i, len(s)-i-1); j <= o && s[i-j] == s[i+j]; j++ {
 			c++
 		}
-
-		if i < len(s)-1 && s[i] == s[i+1] {
-			for j, o := 0, min(i, len(s)-i-2); j <= o && s[i-j] == s[i+j+1]; j++ {
-				c++
-			}
+		for j, o := 0, min(i, len(s)-i-2); j <= o && s[i-j] == s[i+j+1]; j++ {
+			c++
 		}
 	}
 	return
